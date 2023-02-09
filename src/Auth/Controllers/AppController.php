b7a61d4d23e6843d1801a881ed611febb32d32e4
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-02-09 09:46:06 +0300

    -

--- src/Auth/Controllers/AppController.php
+++ src/Auth/Controllers/AppController.php
@@ -2,7 +2,11 @@
 
 namespace App\Modules\Auth\Controllers;
 
+use App\Modules\Auth\Models\Members;
+use App\Modules\Auth\Models\Sessions;
 use App\Modules\Auth\Module;
+use Colibri\App;
+use Colibri\AppException;
 use Colibri\Data\Storages\Storages;
 use Colibri\Web\Controller as WebController;
 use Colibri\Web\PayloadCopy;
@@ -52,4 +56,67 @@ class AppController extends WebController
         );
     }
 
+    /**
+     * Gets an active session for user token
+     * @param RequestCollection $get данные GET
+     * @param RequestCollection $post данные POST
+     * @param mixed $payload данные payload обьекта переданного через POST/PUT
+     * @return object
+     */
+    public function Session(RequestCollection $get, RequestCollection $post, ? PayloadCopy $payload = null): object
+    {
+
+        $code = 200;
+        $result = [];
+        $message = 'Result message';
+        try {
+            
+            $app = Module::$instance->application;
+            if(!$app->params->allowed_ip) {
+                throw new AppException('Not allowed', 403);
+            }
+
+            if($app->params->allowed_ip !== App::$request->remoteip) {
+                throw new AppException('Not allowed', 403);
+            }
+
+            $payloadArray = $payload->ToArray();
+            $token = $payloadArray['token'] ?? $post->token;
+            if(!$token) {
+                throw new AppException('Bad request', 400);
+            }
+
+            $member = Members::LoadByToken($token);
+            if(!$member) {
+                throw new AppException('Member not found', 404);
+            }
+
+            $sessions = Sessions::LoadByMember($member);
+            if(!$sessions || $sessions->Count() == 0) {
+                throw new AppException('Session not found', 404);
+            }
+
+            /** @var \App\Modules\Auth\Models\Session */
+            $session = $sessions->First();
+            $result = $session->ExportForUserInterface();
+
+        } catch (\Throwable $e) {
+            // если что то не так то выводим ошибку
+            $message = $e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine();
+            $code = $e->getCode();
+            App::$log->debug($message);
+        }
+
+        // финишируем контроллер
+        return $this->Finish(
+            $code,
+            $message,
+            $result,
+            'utf-8'
+        );
+
+    }
+
+
+    
 }
